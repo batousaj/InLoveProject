c5b766978f902db9e62b503ad63f79cbe84002ee
Vu Hoang Thien (RBVH/EPS23) <ovt81hc@bosch.com>
2020-12-18 14:41:09 +0700

    No command

---
+++ src/abc.c
@@ -0,0 +1,18 @@
+/**
+ * @file abc.c
+ * @author Thien Vu (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2020-12-18
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ */
+#include <stdio.h>
+#include "abc.h"
+
+void hello_world(void)
+{
+    printf("hello world\n");
+}
+
